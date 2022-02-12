06f7acf2e0c7ca6e7c2d762e87e3a6b23427fc0a
singhanat <singha.osot@gmail.com>
2022-02-12 14:15:06 +0700

    ปรับขนาดไพ่

--- example/example.js
+++ example/example.js
@@ -42,108 +42,108 @@ deck.cards.forEach(function (card, i) {
   card.enableDragging()
   card.enableFlipping()
 
-  card.$el.addEventListener('mousedown', onTouch)
-  card.$el.addEventListener('touchstart', onTouch)
+  // card.$el.addEventListener('mousedown', onTouch)
+  // card.$el.addEventListener('touchstart', onTouch)
 
-  function onTouch () {
-    var card
+  // function onTouch () {
+  //   var card
 
-    if (i % 13 === 0) {
-      acesClicked[i] = true
-      if (acesClicked.filter(function (ace) {
-        return ace
-      }).length === 4) {
-        document.body.removeChild($topbar)
-        deck.$el.style.display = 'none'
-        setTimeout(function () {
-          startWinning()
-        }, 250)
-      }
-    } else if (i % 13 === 12) {
-      if (!kingsClicked) {
-        return
-      }
-      kingsClicked[i] = true
-      if (kingsClicked.filter(function (king) {
-        return king
-      }).length === 4) {
-        for (var j = 0; j < 3; j++) {
-          card = Deck.Card(52 + j)
-          card.mount(deck.$el)
-          card.$el.style[transform] = 'scale(0)'
-          card.setSide('front')
-          card.enableDragging()
-          card.enableFlipping()
-          deck.cards.push(card)
-        }
-        deck.sort(true)
-        kingsClicked = false
-      }
-    } else {
-      acesClicked = []
-      if (kingsClicked) {
-        kingsClicked = []
-      }
-    }
-  }
+  //   if (i % 13 === 0) {
+  //     acesClicked[i] = true
+  //     if (acesClicked.filter(function (ace) {
+  //       return ace
+  //     }).length === 4) {
+  //       document.body.removeChild($topbar)
+  //       deck.$el.style.display = 'none'
+  //       setTimeout(function () {
+  //         startWinning()
+  //       }, 250)
+  //     }
+  //   } else if (i % 13 === 12) {
+  //     if (!kingsClicked) {
+  //       return
+  //     }
+  //     kingsClicked[i] = true
+  //     if (kingsClicked.filter(function (king) {
+  //       return king
+  //     }).length === 4) {
+  //       for (var j = 0; j < 3; j++) {
+  //         card = Deck.Card(52 + j)
+  //         card.mount(deck.$el)
+  //         card.$el.style[transform] = 'scale(0)'
+  //         card.setSide('front')
+  //         card.enableDragging()
+  //         card.enableFlipping()
+  //         deck.cards.push(card)
+  //       }
+  //       deck.sort(true)
+  //       kingsClicked = false
+  //     }
+  //   } else {
+  //     acesClicked = []
+  //     if (kingsClicked) {
+  //       kingsClicked = []
+  //     }
+  //   }
+  // }
 })
 
-function startWinning () {
-  var $winningDeck = document.createElement('div')
-  $winningDeck.classList.add('deck')
+// function startWinning () {
+//   var $winningDeck = document.createElement('div')
+//   $winningDeck.classList.add('deck')
 
-  $winningDeck.style[transform] = translate(Math.random() * window.innerWidth - window.innerWidth / 2 + 'px', Math.random() * window.innerHeight - window.innerHeight / 2 + 'px')
+//   $winningDeck.style[transform] = translate(Math.random() * window.innerWidth - window.innerWidth / 2 + 'px', Math.random() * window.innerHeight - window.innerHeight / 2 + 'px')
 
-  $container.appendChild($winningDeck)
+//   $container.appendChild($winningDeck)
 
-  var side = Math.floor(Math.random() * 2) ? 'front' : 'back'
+//   var side = Math.floor(Math.random() * 2) ? 'front' : 'back'
 
-  for (var i = 0; i < 55; i++) {
-    addWinningCard($winningDeck, i, side)
-  }
+//   for (var i = 0; i < 55; i++) {
+//     addWinningCard($winningDeck, i, side)
+//   }
 
-  setTimeout(startWinning, Math.round(Math.random() * 1000))
-}
+//   setTimeout(startWinning, Math.round(Math.random() * 1000))
+// }
 
-function addWinningCard ($deck, i, side) {
-  var card = Deck.Card(54 - i)
-  var delay = (55 - i) * 20
-  var animationFrames = Deck.animationFrames
-  var ease = Deck.ease
+// function addWinningCard ($deck, i, side) {
+//   var card = Deck.Card(54 - i)
+//   var delay = (55 - i) * 20
+//   var animationFrames = Deck.animationFrames
+//   var ease = Deck.ease
 
-  card.enableFlipping()
+//   card.enableFlipping()
 
-  if (side === 'front') {
-    card.setSide('front')
-  } else {
-    card.setSide('back')
-  }
+//   if (side === 'front') {
+//     card.setSide('front')
+//   } else {
+//     card.setSide('back')
+//   }
 
-  card.mount($deck)
-  card.$el.style.display = 'none'
+//   card.mount($deck)
+//   card.$el.style.display = 'none'
 
-  var xStart = 0
-  var yStart = 0
-  var xDiff = -500
-  var yDiff = 500
+//   var xStart = 0
+//   var yStart = 0
+//   var xDiff = -500
+//   var yDiff = 500
 
-  animationFrames(delay, 1000)
-    .start(function () {
-      card.x = 0
-      card.y = 0
-      card.$el.style.display = ''
-    })
-    .progress(function (t) {
-      var tx = t
-      var ty = ease.cubicIn(t)
-      card.x = xStart + xDiff * tx
-      card.y = yStart + yDiff * ty
-      card.$el.style[transform] = translate(card.x + 'px', card.y + 'px')
-    })
-    .end(function () {
-      card.unmount()
-    })
-}
+//   animationFrames(delay, 1000)
+//     .start(function () {
+//       card.x = 0
+//       card.y = 0
+//       card.$el.style.display = ''
+//     })
+//     .progress(function (t) {
+//       var tx = t
+//       var ty = ease.cubicIn(t)
+//       card.x = xStart + xDiff * tx
+//       card.y = yStart + yDiff * ty
+//       card.$el.style[transform] = translate(card.x + 'px', card.y + 'px')
+//     })
+//     .end(function () {
+//       card.unmount()
+//     })
+// }
 
 // easter eggs end
 
@@ -185,48 +185,48 @@ deck.sort()
 
 // secret message..
 
-var randomDelay = 10000 + 30000 * Math.random()
+// var randomDelay = 10000 + 30000 * Math.random()
 
-setTimeout(function () {
-  printMessage('Psst..I want to share a secret with you...')
-}, randomDelay)
+// setTimeout(function () {
+//   printMessage('Psst..I want to share a secret with you...')
+// }, randomDelay)
 
-setTimeout(function () {
-  printMessage('...try clicking all kings and nothing in between...')
-}, randomDelay + 5000)
+// setTimeout(function () {
+//   printMessage('...try clicking all kings and nothing in between...')
+// }, randomDelay + 5000)
 
-setTimeout(function () {
-  printMessage('...have fun ;)')
-}, randomDelay + 10000)
+// setTimeout(function () {
+//   printMessage('...have fun ;)')
+// }, randomDelay + 10000)
 
-function printMessage (text) {
-  var animationFrames = Deck.animationFrames
-  var ease = Deck.ease
-  var $message = document.createElement('p')
-  $message.classList.add('message')
-  $message.textContent = text
+// function printMessage (text) {
+//   var animationFrames = Deck.animationFrames
+//   var ease = Deck.ease
+//   var $message = document.createElement('p')
+//   $message.classList.add('message')
+//   $message.textContent = text
 
-  document.body.appendChild($message)
+//   document.body.appendChild($message)
 
-  $message.style[transform] = translate(window.innerWidth + 'px', 0)
+//   $message.style[transform] = translate(window.innerWidth + 'px', 0)
 
-  var diffX = window.innerWidth
+//   var diffX = window.innerWidth
 
-  animationFrames(1000, 700)
-    .progress(function (t) {
-      t = ease.cubicInOut(t)
-      $message.style[transform] = translate((diffX - diffX * t) + 'px', 0)
-    })
+//   animationFrames(1000, 700)
+//     .progress(function (t) {
+//       t = ease.cubicInOut(t)
+//       $message.style[transform] = translate((diffX - diffX * t) + 'px', 0)
+//     })
 
-  animationFrames(6000, 700)
-    .start(function () {
-      diffX = window.innerWidth
-    })
-    .progress(function (t) {
-      t = ease.cubicInOut(t)
-      $message.style[transform] = translate((-diffX * t) + 'px', 0)
-    })
-    .end(function () {
-      document.body.removeChild($message)
-    })
-}
+//   animationFrames(6000, 700)
+//     .start(function () {
+//       diffX = window.innerWidth
+//     })
+//     .progress(function (t) {
+//       t = ease.cubicInOut(t)
+//       $message.style[transform] = translate((-diffX * t) + 'px', 0)
+//     })
+//     .end(function () {
+//       document.body.removeChild($message)
+//     })
+// }
